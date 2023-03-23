bc93f932e3bce21fd07603d9021685b022f2e802
Bishal <bishalbera2@gmail.com>
2023-03-23 21:17:48 +0530

    score card added

--- app/src/main/java/com/example/biggernumberapp/MainActivity.kt
+++ app/src/main/java/com/example/biggernumberapp/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.biggernumberapp
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 
 import java.util.*
 
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var leftbutton: Button
     private lateinit var rightbutton: Button
     private lateinit var background: ConstraintLayout
-
+    private lateinit var scorecounter: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         leftbutton = findViewById(R.id.btnLeft)
         rightbutton = findViewById(R.id.btnRight)
         background = findViewById(R.id.backgroundView)
+        scorecounter = findViewById(R.id.Scorecounter)
         assignNumbersToButtons()
         leftbutton.setOnClickListener {
             checkAnswer(true)
@@ -32,7 +36,9 @@ class MainActivity : AppCompatActivity() {
             checkAnswer(false)
             assignNumbersToButtons()
         }
+
     }
+
 
     private fun assignNumbersToButtons() {
         val r = Random()
@@ -45,21 +51,37 @@ class MainActivity : AppCompatActivity() {
         rightbutton.text = "$num2"
     }
 
+    @SuppressLint("SetTextI18n")
     private fun checkAnswer(isLeftButtonSelected: Boolean) {
         val n1 = leftbutton.text.toString().toInt()
         val n2 = rightbutton.text.toString().toInt()
-        val isAnswerCorrect = if (isLeftButtonSelected) n1 > n2 else n2 > n1
+        var isAnswerCorrect = if (isLeftButtonSelected) n1 > n2 else n2 > n1
         val toastMessage: String
         val backgroundColor: Int
+
+        var finalscore =  0
+
         if (isAnswerCorrect) {
             toastMessage = "Correct!!"
             backgroundColor = Color.GREEN
+
+            scorecounter.text = "score${finalscore.inc()}"
         } else {
             toastMessage = "Wrong"
             backgroundColor = Color.RED
+            scorecounter.text = "score:${finalscore.dec()}"
         }
+
         Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
         background.setBackgroundColor(backgroundColor)
 
     }
+
+
 }
+
+
+
+
+
+
